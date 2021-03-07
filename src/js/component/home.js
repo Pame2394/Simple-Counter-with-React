2322059adee69876028f099beb0464ee4ae919e2
Pamela Contreras <pame2394@gmail.com>
2021-03-07 21:56:59 +0000

    listo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,26 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import PropTypes from "prop-types";
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bigCounter">
+			<div className="clock">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="six"> {props.dSix % 10} </div>
+			<div className="five"> {props.dFive % 10} </div>
+			<div className="four"> {props.dFour % 10} </div>
+			<div className="three"> {props.dThree % 10} </div>
+			<div className="two"> {props.dTwo % 10} </div>
+			<div className="one"> {props.dOne % 10} </div>
 		</div>
 	);
 }
+Home.propTypes = {
+	dSix: PropTypes.number,
+	dFive: PropTypes.number,
+	dFour: PropTypes.number,
+	dThree: PropTypes.number,
+	dTwo: PropTypes.number,
+	dOne: PropTypes.number
+};
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,29 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
+
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function() {
+	const six = Math.floor(counter / 100000);
+	const five = Math.floor(counter / 10000);
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(six, five, four, three, two, one);
+	counter++;
+	ReactDOM.render(
+		<Home
+			dSix={six}
+			dFive={five}
+			dFour={four}
+			dThree={three}
+			dTwo={two}
+			dOne={one}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
